d6221adbbd52214655e45afc6f61999372d1dfad
red-jen <redaje102105@gmail.com>
2025-03-08 23:02:10 +0000

    updatting product controller to manage reviews

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -10,21 +10,37 @@ class ProductsController extends Controller
 {
     public function index(Request $request)
     {
-        $query = Product::where('is_active', true);
+        $query = Product::query();
         
         // Filter by category if provided
         if ($request->has('category')) {
             $query->where('category_id', $request->category);
         }
         
-        $products = $query->get();
+        $products = $query->with('images')->get();
         $categories = Category::all();
         
-        return view('products.index', compact('products', 'categories'));
+        // Get cart for the cart counter
+        $cart = app(CartController::class)->getCart();
+        
+        return view('products.index', compact('products', 'categories', 'cart'));
     }
     
     public function show(Product $product)
     {
-        return view('products.show', compact('product'));
+        $product->load('images', 'category', 'reviews.user');
+        
+        // Get related products
+        $relatedProducts = Product::where('category_id', $product->category_id)
+            ->where('id', '!=', $product->id)
+            ->with('images')
+            ->take(3)
+            ->get();
+            
+        // Get cart for the cart counter
+        $cart = app(CartController::class)->getCart();
+        
+        return view('products.show', compact('product', 'relatedProducts', 'cart'));
     }
+    
 }
