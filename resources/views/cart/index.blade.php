6c7d6417562edc3829b78d25d4a83cccef2965cf
red-jen <redaje102105@gmail.com>
2025-03-11 11:02:06 +0000

    cart display money

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -86,7 +86,7 @@
                                 </x-secondary-button>
                             </form>
                             
-                            <a href="{{ route('checkout') }}" class="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700">
+                            <a href="{{ route('checkout.index') }}" class="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700">
                                 Proceed to Checkout
                             </a>
                         </div>
